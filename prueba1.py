9f1224a2ed7514239659cdcb02d5acc436f11c53
ricardoCea <ricardo.cea@genstudents.org>
2024-08-08 21:05:23 -0400

    agrega hola pos

--- prueba1.py
+++ prueba1.py
@@ -1,4 +1,4 @@
 name = input("What is your name? ")
 age = int(input("How old are you? "))
 print("tu nombre es " + name + " y tu edad es " + str(age) + ". Programa terminado.")
-# holi
+# holi pos hehe
